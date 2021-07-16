ba4f16f5b14f147d6dbd169d85b431e52a8d4cb5
Sarthak Saklecha <sarthaksaklecha1@gmail.com>
2021-07-17 01:25:31 +0530

    create posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,19 +1,28 @@
 import postMessage from "../models/postMessage.js";
 
-export const getPosts = (req,res) => {
-    postMessage.find()
-    .then((posts) => res.json(posts))
-    .catch((err) => res.status(400).send(err))
+export const getPosts = async (req,res) => {
+    try{
+        let posts = await postMessage.find()
+        posts = posts.json();
+    }catch(err){
+        res.status("404").send(err.message)
+    }
 }
 
-export const createPosts = (req, res) => {
-    const post = new postMessage({
-        title : "first post",
-        message : "bananas are the best",
-        creator: "leslie chow",
-        potty : "muddy"
+export const createPosts = async (req, res) => {
+    console.log(req.body);
+    const { title,message } = req.body;
+    const newPost = new postMessage({
+        title : title,
+        message : message,
+        creator: "leslie chow"
     });
-    post.save();
-    res.send(post)
+    try{
+        await newPost.save();
+        res.status(201).json(newPost);
+    }catch(err){
+        res.status(400).json(err.message);
+    }
+    
 }
 
